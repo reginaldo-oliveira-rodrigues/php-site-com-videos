78891eb0f5797275a7ee4141d9af61a91576ac8c
reginaldo-oliveira-rodrigues <regisolliver1@gmail.com>
2014-03-04 15:22:38 -0300

    tornando mais flexível a indexação do conjunto de vídeos.

--- js/videos.js
+++ js/videos.js
@@ -1,10 +1,10 @@
 function get_conjunto_de_videos(){
     var videos = new Array();
-    
+    var indice = 0;
     // bakuman 3 episodios 24 e 25 (final)
-    videos[0] = "http://174.127.85.80/c0d73ce87d5086b7a9be644f94173830/531489f9/55297.mp4";
-    videos[1] = "http://174.127.85.72/aa74482b728f2ef88125ddfe5e87554b/531411f5/55476.mp4";
-    videos[2] = "http://174.127.85.80/f61329ee7aae73325cc9e8b85cd9a070/531411fc/56093.mp4";
+    videos[ indice++ ] = "http://174.127.85.80/c0d73ce87d5086b7a9be644f94173830/531489f9/55297.mp4";
+    videos[ indice++ ] = "http://174.127.85.72/aa74482b728f2ef88125ddfe5e87554b/531411f5/55476.mp4";
+    videos[ indice++ ] = "http://174.127.85.80/f61329ee7aae73325cc9e8b85cd9a070/531411fc/56093.mp4";
         
     return videos;
 }
